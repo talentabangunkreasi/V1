4daeccad78aaf2c7e14aa57dc3b0039739b75be5
talentabangunkreasi <talentabangunkreasi.gps@gmail.com>
2026-05-15 12:47:52 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function HomePage() {
           setTimeout(() => {
             window.scrollTo({ top: 0, behavior: 'smooth' })
             setActiveSection('home')
-          }, 900)
+          }, 2000)
 
         }, 300)
 
